7f9e12e2c57994fe8c18705dc883c1b825ce6599
shouya31 <shouya19980331@gmail.com>
2019-01-14 19:20:53 +0900

    ユーザー新規登録、ログインのマークアップ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+	def signin_form
+		
+	end
+
+	def adress
+		
+	end
+
+	def credit
+		
+	end
+
+	def finish
+		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root "markets#index"
   resources :markets, only: [:index]
+  get 'users/signin_form' => 'users#signin_form'
+  get 'users/adress' => 'users#adress'
+  get 'users/credit' => 'users#credit'
+  get 'users/finish' => 'users#finish'
+  
 end
