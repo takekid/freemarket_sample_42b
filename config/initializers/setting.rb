ec3873dca5678cc1430ff7fcc979f31a060311de
shouya31 <shouya19980331@gmail.com>
2019-01-18 22:06:00 +0900

    修正

--- config/initializers/setting.rb
+++ config/initializers/setting.rb
@@ -1,3 +1,3 @@
 default_payjp:
-  PAYJP_PUBLIC_KEY = 'pk_test_4471c89aae0733fc8aba044d'
-  PAYJP_SECRET_KEY = 'sk_test_33ed6ec1e16acea820f30a16'
+  PAYJP_PUBLIC_KEY = '<%= ENV['PAYJP_PUBLIC_KEY'] %>' 
+  PAYJP_SECRET_KEY =  '<%= ENV['PAYJP_SECRET_KEY'] %>' 
