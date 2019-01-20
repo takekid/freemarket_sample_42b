11d01baa8d58ce084ddf6f2ebe44ff0f95e3c80a
Ryotaman213 <ryotafreeman@gmail.com>
2019-01-20 16:30:53 +0900

    seed.fuの導入、Item周りのデータ作成

--- db/fixtures/development/06_item.rb
+++ db/fixtures/development/06_item.rb
@@ -10,3 +10,55 @@ s.price = '30000'
 s.status = '販売中'
 s.seller_id = 1
 end
+
+Item.seed do |s|
+s.id = 2
+s.name = 'ダミーです2'
+s.introduction = 'ダミーの商品です2'
+s.condition = '新品・未使用'
+s.shippingfee = '送料込み'
+s.shipfrom = '大阪府'
+s.shipping_date = '2~3日で発送'
+s.price = '30000'
+s.status = '販売中'
+s.seller_id = 1
+end
+
+Item.seed do |s|
+s.id = 1
+s.name = 'ダミーです'
+s.introduction = 'ダミーの商品です'
+s.condition = '新品・未使用'
+s.shippingfee = '送料込み'
+s.shipfrom = '大阪府'
+s.shipping_date = '2~3日で発送'
+s.price = '30000'
+s.status = '販売中'
+s.seller_id = 1
+end
+
+Item.seed do |s|
+s.id = 3
+s.name = 'ダミーです3'
+s.introduction = 'ダミーの商品です3'
+s.condition = '新品・未使用'
+s.shippingfee = '送料込み'
+s.shipfrom = '大阪府'
+s.shipping_date = '2~3日で発送'
+s.price = '30000'
+s.status = '販売中'
+s.seller_id = 1
+end
+
+Item.seed do |s|
+s.id = 4
+s.name = 'ダミーです4'
+s.introduction = 'ダミーの商品です4'
+s.condition = '新品・未使用'
+s.shippingfee = '送料込み'
+s.shipfrom = '大阪府'
+s.shipping_date = '2~3日で発送'
+s.price = '30000'
+s.status = '販売中'
+s.seller_id = 1
+end
