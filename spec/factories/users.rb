b24c0dae26dee1acc5a74948f329ca0567322517
shouya31 <shouya19980331@gmail.com>
2019-01-21 10:24:27 +0900

    Test

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :user do
+    nickname                "shouya"
+    email                   "aaa@aaa"
+    password                "123456"
+    password_confirmation   "123456"
+    name                     "huhhh"
+    kana_name                "yamada"
+  end
+end
