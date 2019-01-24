ba2e128676c53b87d57bbbf4ea9d4b5f8c63412b
takekid <chikeureca@yahoo.co.jp>
2019-01-24 19:51:05 +0900

    出品時のモデル周り変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,9 @@ class User < ApplicationRecord
   has_many :addresses
   has_many :items
 
+  has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
+  has_many :salling_items, -> { where("buyer_id is NULL") }, foreign_key: "seller_id", class_name: "Item"
+  has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "seller_id", class_name: "Item"
+
 
 end
