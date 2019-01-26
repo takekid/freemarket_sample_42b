b38b20752ef9289e8f69e4aa97f9148478e24d43
takekid <chikeureca@yahoo.co.jp>
2019-01-26 12:38:42 +0900

    出品機能（途中）

--- app/assets/javascripts/item_sell.js
+++ app/assets/javascripts/item_sell.js
@@ -5,4 +5,16 @@ $(document).on('turbolinks:load',function(){
     else
       $('#chi').css('display','none');
   });
+  function readURL(input) {
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          $('#item-prev').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+        }
+      }
+      $("#item_item_images_attributes_0_image").change(function(){
+         readURL(this);
+ });
 });
