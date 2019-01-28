b19e0c1b9ccc56c578663fafd7ec4267fb2b2925
Ryotaman213 <ryotafreeman@gmail.com>
2019-01-28 14:19:50 +0900

    デプロイ時に初期データの投入
    # What
    本番環境の挙動確認のため、初期データの投入
    キャピストラーノで自動デプロイ時にseed_fuを実行するように記入しております。
    ローカル環境ではcsvファイル含めて、保存できております。
    # Why
    本番環境での仮データの挙動を確かめたいため。

---
+++ db/fixtures/development/00_user.rb
@@ -0,0 +1,4 @@
+User.create!(id: 1, nickname: 'メルカリ太郎', email: 'sample@test.com',
+password: 'password', password_confirmation: 'password')
+User.create!(id: 2, nickname: 'メルカリ二郎', email: 'sample2@test.com',
+password: 'password', password_confirmation: 'password')
