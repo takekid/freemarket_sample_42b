e07543429b07b12a72d8f744034f069871df8caf
Ryotaman213 <ryotafreeman@gmail.com>
2019-01-30 16:36:05 +0900

    初期データ

--- db/fixtures/production/05_category.rb
+++ db/fixtures/production/05_category.rb
@@ -5,6 +5,10 @@ csv = CSV.read('db/fixtures/production/category.csv')
     Category.seed do |s|
     s.id = category[0]
     s.category_name = category[1]
+    s.created_at = category[2]
+    s.updated_at = category[3]
     s.parent_id = category[4]
+    s.lft = category[5]
+    s.rigt = category[6]
     end
 end
